615e5e84124e77f1b1d4cea28f0e9bae2db1a0b7
balojr <77960542+balojr@users.noreply.github.com>
2023-04-01 21:15:46 +0300

    Add item service

--- portal/src/app/services/item-service.service.ts
+++ portal/src/app/services/item-service.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ItemData } from '../models/item-model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ItemServiceService {
+export class ItemService {
+  constructor( private httpClient: HttpClient) { }
 
-  constructor() { }
+  private baseUrl = environment.API_ENDPOINT + "/api"
+
+  getOneItem(id: number): Observable<any> {
+    return this.httpClient.get<any>(this.baseUrl + '/item/' + id, {
+      // params: options,
+      observe: 'response',
+    });
+  }
 }
