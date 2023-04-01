d6e4b5653ec5587d9f212c6e5fc66309b3e7ee8e
balojr <77960542+balojr@users.noreply.github.com>
2023-04-01 21:15:17 +0300

    Add item functions

--- portal/src/app/components/items/items.component.ts
+++ portal/src/app/components/items/items.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from 'src/app/services/item-service.service';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.scss']
 })
-export class ItemsComponent {
+export class ItemsComponent  implements OnInit{
+  items!: any;
+
+
+  constructor(private http: HttpClient, public itemService: ItemService) { }
+
+  ngOnInit(): void {
+    
+  }
+  viewItem(id: number) {
+    const options = {
+      //  page: 1,
+      //  per_page: 10,
+      //  home_id: this.patientId,
+    };
+    this.itemService.getOneItem(id).subscribe((res: any) => {
+      console.log('item=>', res.body.data);
+      this.items = res.body.data;
+
+    })
+  }
 
 }
